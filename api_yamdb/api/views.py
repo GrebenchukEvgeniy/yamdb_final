0d322ff9cf1acb763130b607b23104eaaa001020
GrebenchukEvgeniy <grebenchuk.evgeniy@gmail.com>
2022-10-31 21:41:55 +0500

    isort

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,9 @@
+from api.permissions import AuthorOrAdminOrReadOnly
+from api.serializers import CommentSerializer, ReviewSerializer
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
-
-from api.permissions import AuthorOrAdminOrReadOnly
-from api.serializers import CommentSerializer, ReviewSerializer
 from reviews.models import Category, Genre, Review, Title
 
 from .firters import TitlesFilter
